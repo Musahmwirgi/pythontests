54b2e4ea76f8b868f9a415abda9560d9cb507238
Musahmwirgi <36617782+Musahmwirgi@users.noreply.github.com>
2018-03-07 11:54:09 +0300

    Add files via upload

---
+++ test1.py
@@ -0,0 +1,23 @@
+from datetime import datetime
+import calendar
+
+print('Enter Name:')
+name = input()
+print('name', name)
+
+print('Enter Birth date (in the format dd-mm-yyyy:')
+birth_date = input()
+print('Birth date', birth_date)
+
+print('Age at eight five:')
+born = birth_date.split('-')
+age_at_eighty_five = int(born[2]) + 85
+print(born[0], '-', born[1], '-',  age_at_eighty_five)
+
+print('Day of Birth:')
+
+datetime.strptime(birth_date, '%d-%m-%Y').weekday()
+
+print(calendar.day_name[datetime.strptime(birth_date, '%d-%m-%Y').weekday()])
+
+print('Hi', name, 'you will turn 85 on', born[0],'-',born[1],'-',age_at_eighty_five,'.','You were born on', calendar.day_name[datetime.strptime(birth_date, '%d-%m-%Y').weekday()] )
